0e09b6fed06860342db4ffba5f5a4f07756c58b3
Mayank Vishwakarma <mayankat03@gmail.com>
2021-08-08 14:50:43 +0530

    local changes

---
+++ scripts/script1.js
@@ -0,0 +1,21 @@
+$.getJSON("https://api.covid19india.org/data.json", function(data){
+    // console.log(data);
+    var today = data.cases_time_series[data.cases_time_series.length - 1];
+    var yesterday = data.cases_time_series[data.cases_time_series.length - 2];
+    // console.log(today);
+    // console.log(yesterday);
+    $(".todaydate").append(today.date);
+    $(".yesterdaydate").append(yesterday.date);
+    $(".todaycases").append(today.dailyconfirmed);
+    $(".todayrecovered").append(today.dailyrecovered);
+    $(".todayactive").append(today.dailyconfirmed - today.dailydeceased - today.dailyrecovered );
+    $(".todaydeaths").append(today.dailydeceased);
+})
+
+$.getJSON("https://api.rootnet.in/covid19-in/stats/latest", function(latest){
+    // console.log(latest.data.summary.total);
+    $(".totalcases").append(latest.data.summary.total);
+    $(".totalrecovered").append(latest.data.summary.discharged);
+    $(".totalactive").append(latest.data.summary.total - (latest.data.summary.discharged - latest.data.summary.deaths));
+    $(".totaldeaths").append(latest.data.summary.deaths);
+})
