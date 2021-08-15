0e340c7e67bafc49d0261c05b3d1c5ddb23aaefb
Anurag Dangi <anuragdangi015@gmail.com>
2021-08-15 17:00:58 +0530

    successfully updated the uverview world

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,10 +1,10 @@
-// total overview
+// total india overview
 $.getJSON('https://api.rootnet.in/covid19-in/stats/latest', function (latest) {
   // console.log(latest.data.summary.total);
   $('.totalcases').append(latest.data.summary.total);
   $('.totalrecovered').append(latest.data.summary.discharged);
   $('.totalactive').append(
-    latest.data.summary.total - (latest.data.summary.discharged - latest.data.summary.deaths)
+    latest.data.summary.total - (latest.data.summary.discharged + latest.data.summary.deaths)
   );
   $('.totaldeaths').append(latest.data.summary.deaths);
 });
@@ -24,4 +24,13 @@ $.getJSON('https://api.covid19india.org/data.json', function (data) {
     today.dailyconfirmed - today.dailydeceased - today.dailyrecovered
   );
   $('.todaydeaths').append(today.dailydeceased);
+});
+
+// total overview world
+$.getJSON('https://api.covid19api.com/summary', function (all) {
+  // console.log(all.Global);
+  $('.totalcasesw').append(all.Global.TotalConfirmed);
+  $('.totalrecoveredw').append(all.Global.TotalRecovered);
+  $('.totalactivew').append(all.Global.TotalConfirmed - all.Global.TotalRecovered - all.Global.TotalDeaths);
+  $('.totaldeathsw').append(all.Global.TotalDeaths);
 });
